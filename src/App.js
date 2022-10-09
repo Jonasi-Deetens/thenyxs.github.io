735cf9f05ecfb141dca86eecabcac3fb302c71fc
TheNyxs <jonasideetens@hotmail.com>
2022-10-09 23:46:32 +0200

    Added navbar demo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navigation/Navbar.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-		<p>
-          This means everything is working.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		    <nav>
+
+        </nav>
       </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
