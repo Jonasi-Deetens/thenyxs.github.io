cdbc8b9f7fd8fee4eb4f0ecf9265e4d2b1632cea
TheNyxs <jonasideetens@hotmail.com>
2022-10-09 23:51:52 +0200

    Added navbar demo

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-		    <nav>
-
-        </nav>
+        <div>
+          <Navbar />
+        </div>;
       </header>
       <main>
 
