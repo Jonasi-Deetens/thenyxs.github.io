a9d35a22e59380f6887117c1e6105c8aba61fbd1
TheNyxs <jonasideetens@hotmail.com>
2022-10-09 03:38:31 +0200

    Added test message to the index 'App.js'

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+		<p>
+          This means everything is working.
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
