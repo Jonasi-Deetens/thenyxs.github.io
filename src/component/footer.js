b199e15339db9f5d581581ca95d8632a512129e6
TheNyxs <jonasideetens@hotmail.com>
2022-10-11 01:32:49 +0200

    Add a scroll to top after pressing a footer link.

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollToTop from '../navigation/scrollToTop';
 
 import {
     BrowserRouter as Router,
@@ -9,13 +10,15 @@ const Footer = () => {
     return (
         <div>
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-            <li><Link to="/home" className="nav-item nav-link px-2 text-muted" href="#">Home</Link></li>
-            <li><Link to="/about" className="nav-item nav-link px-2 text-muted" href="#">About us</Link></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
+                <li className="nav-item" ><Link to="/home" className="nav-link px-2 text-muted" href="#">Home</Link></li>
+                <li className="nav-item" ><Link to="/about" className="nav-link px-2 text-muted" href="#">About us</Link></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
             </ul>
             <p className="text-center text-muted">© 2021 Company, Inc</p>
+            
+            <ScrollToTop />
         </div>
     );
 };
