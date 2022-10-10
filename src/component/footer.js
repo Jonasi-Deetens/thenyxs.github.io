adbffd868ffbbf39c61598b09349ff4730c58ed0
TheNyxs <jonasideetens@hotmail.com>
2022-10-11 00:39:46 +0200

    Changed footer links

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import Link from 'react-router-dom';
+
 const Footer = () => {
     return (
         <div>
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
+            <li><Link to="/" className="dropdown-item" href="#">Home</Link></li>
+            <li><Link to="/about" className="dropdown-item" href="#">About us</Link></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
             </ul>
             <p className="text-center text-muted">© 2021 Company, Inc</p>
         </div>
