7995f9b4cbd015d16d186e15a446a1a1eae0a477
TheNyxs <jonasideetens@hotmail.com>
2022-10-11 00:43:30 +0200

    Changed footer links

--- src/component/footer.js
+++ src/component/footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
     return (
         <div>
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-            <li><Link to="/" className="dropdown-item" href="#">Home</Link></li>
-            <li><Link to="/about" className="dropdown-item" href="#">About us</Link></li>
+            <li><Link to="/" className="nav-item nav-link px-2 text-muted" href="#">Home</Link></li>
+            <li><Link to="/about" className="nav-item nav-link px-2 text-muted" href="#">About us</Link></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
