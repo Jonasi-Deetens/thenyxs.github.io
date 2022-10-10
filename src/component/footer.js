5d894641bfa6e6c1c49f81517c753bf11e8c0dd8
TheNyxs <jonasideetens@hotmail.com>
2022-10-11 00:41:28 +0200

    Added footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import Link from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Link
+  } from 'react-router-dom';
 
 const Footer = () => {
     return (
