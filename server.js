4d01865e15ba3fb6c20a33e81f6edd7e30ef1329
TheNyxs <jonasideetens@hotmail.com>
2022-10-11 03:15:15 +0200

    Adding MYSQL Support

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const cors = require('cors');
 
 const db = mysql.createPool({
-    host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
-    user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
-    password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
-    database: 'books' // database name MYSQL_HOST_IP: mysql_db
+    host: 'localhost', // the host name MYSQL_DATABASE: node_mysql
+    user: 'react', // database user MYSQL_USER: MYSQL_USER
+    password: 'Fl4shback321!', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
+    database: 'reactdb' // database name MYSQL_HOST_IP: mysql_db
   })
 
 const app = express();
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hi There')
+    
   });
 
 app.get('/get', (req, res) => {
