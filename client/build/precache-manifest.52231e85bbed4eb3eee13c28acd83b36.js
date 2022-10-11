0af4ac3084772f48927e9d1e76eed0313ae808a0
TheNyxs <jonasideetens@hotmail.com>
2022-10-12 00:37:36 +0200

    Trying to connect front to back-end

---
+++ client/build/precache-manifest.52231e85bbed4eb3eee13c28acd83b36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "47ea47b9d8b640c31e52",
+    "url": "/static/css/main.6ee43935.chunk.css"
+  },
+  {
+    "revision": "47ea47b9d8b640c31e52",
+    "url": "/static/js/main.47ea47b9.chunk.js"
+  },
+  {
+    "revision": "10e59a8baf6e9a2d1eea",
+    "url": "/static/js/1.10e59a8b.chunk.js"
+  },
+  {
+    "revision": "88e6076075cc2c4d25cc",
+    "url": "/static/css/2.31facaf8.chunk.css"
+  },
+  {
+    "revision": "88e6076075cc2c4d25cc",
+    "url": "/static/js/2.88e60760.chunk.js"
+  },
+  {
+    "revision": "3e778183cb43d2725271",
+    "url": "/static/js/runtime~main.3e778183.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9560b266cb52515c892f2f4797ded558",
+    "url": "/index.html"
+  }
+];
---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/reactDB');
+
+const bookSchema = new mongoose.Schema({
+  title: String,
+  review: String
+});
+
+const Book = new mongoose.model("Book", bookSchema);
+
+const book1 = new Book({
+  title: "Test title1",
+  review: "This is awesome"
+});
+
+const book2 = new Book({
+  title: "Test title2",
+  review: "This is 2 awesome"
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+
+  Book.find({}, function(err, foundBooks) {
+
+    if (foundBooks.length === 0) {
+      Book.insertMany([book1, book2], function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Succesfully inserted Books.")
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.json({
+        books: foundBooks
+      })
+    }
+  });
+
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port: 3000');
+});
