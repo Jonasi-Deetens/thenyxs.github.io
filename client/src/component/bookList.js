f4bbae8bde24ec0a7e543d16056baa30f8eb5fbb
TheNyxs <jonasideetens@hotmail.com>
2022-10-12 19:12:25 +0200

    back to fron

--- client/src/component/bookList.js
+++ client/src/component/bookList.js
@@ -3,19 +3,24 @@ import { useState, useEffect } from 'react';
   
 const BookList = () => {
     const [books, setBooks] = useState({}); 
+    const items = [];
 
     useEffect(() => {
         fetch("/api/books")
-        .then(res => res.json())
-        .then(books => setBooks(books))
-        console.log(books);
-    }, []);
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    });
 
     return (
-    <div>
-        <p>{books.title}</p>
-        <p>{books.review}</p>
-    </div>
+        <div>
+            <p>{items[0].title}</p>
+            <p>{items.review}</p>
+        </div>
     );
 }
 export default BookList;
